a770871371f0590248d33d4aa3d775623fcb32c2
Preston Monroe <69488647+RedneckLovinLife@users.noreply.github.com>
2023-03-10 22:11:43 -0500

    Update program.lua

--- program.lua
+++ program.lua
@@ -1,3 +1,20 @@
+RegisterCommand("cc2", function() 
+    local ped = PlayerPedId()
+    local coords = GetEntityCoords(ped)
+
+    if IsPedInAnyVehicle(ped) then 
+        local vehicle = GetVehiclePedIsIn(ped)
+        coords = GetEntityCoords(vehicle)
+    end
+
+    local message = {
+        type = "copyCoordsV2",
+        x = coords.x,
+        y = coords.y,
+    }
+    SendNuiMessage(json.encode(message))
+end)
+
 RegisterCommand("cc3", function() 
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
@@ -58,20 +75,41 @@ end)
 RegisterCommand("ccg2", function() 
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
-    local heading = GetEntityHeading(ped)    
+    local heading = GetEntityHeading(ped)
 
     if IsPedInAnyVehicle(ped) then 
         local vehicle = GetVehiclePedIsIn(ped)
         coords = GetEntityCoords(vehicle)
-        heading = GetEntityHeading(vehicle)    
+        heading = GetEntityHeading(vehicle)
     end
 
     local message = {
         type = "copyCoordsGarage2",
         x = coords.x,
         y = coords.y,
-        z = coords.z
+        z = coords.z,
+        h = heading
     }
     SendNuiMessage(json.encode(message))
 end)
 
+RegisterCommand("cct", function() 
+    local ped = PlayerPedId()
+    local coords = GetEntityCoords(ped)
+    local heading = GetEntityHeading(ped)
+    
+    if IsPedInAnyVehicle(ped) then 
+        local vehicle = GetVehiclePedIsIn(ped)
+        coords = GetEntityCoords(vehicle)
+        heading = GetEntityHeading(vehicle)
+    end
+
+    local message = {
+        type = "copyCoordsTrucking",
+        x = coords.x,
+        y = coords.y,
+        z = coords.z,
+        h = heading
+    }
+    SendNuiMessage(json.encode(message))
+end)
